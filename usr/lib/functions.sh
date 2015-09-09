7e930de2881c549ba7aa66dd1a99f77eb56fc2be
Brian Elliott Finley <brian@thefinleys.com>
2015-09-08 21:52:17 -0500

    - support installation on debian and RPM based systems - improve handling of default (best practice) settings - provide easy way of creating documented config file - improve user guidance w/out requiring reading any documentation - auto-detect scaling drivers: intel_pstate, acpi-cpufreq  and behave appropriately for each - improve some variable and function names to better represent their purpose - take values directly from kernel table whenever possible, rather than deriving them

--- usr/lib/functions.sh
+++ usr/lib/functions.sh
@@ -9,49 +9,110 @@ SEQ=$(which seq)
 C1EUTIL=$(which c1eutil)
 SET_DMA_LATENCY=$(which set_dma_latency)
 
+acpi_cpufreq_SCALING_AVAILABLE_FREQUENCIES_file="/sys/devices/system/cpu/cpu0/cpufreq/scaling_available_frequencies"
+SCALING_DRIVER_file="/sys/devices/system/cpu/cpu0/cpufreq/scaling_driver"
+INTEL_PSTATE_NO_TURBO_file="/sys/devices/system/cpu/intel_pstate/no_turbo"
+CPUINFO_MAX_FREQ_file="/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq"
+CPUINFO_MIN_FREQ_file="/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_min_freq"
+
+
+get_CPUFREQ_SCALING_DRIVER() {
+    if [ -e $SCALING_DRIVER_file ]; then
+        my_SCALING_DRIVER=$(cat $SCALING_DRIVER_file)
+            #
+            #   Possible scaling drivers as of 2015.09.07:
+            #
+            #   intel_pstate
+            #   acpi-cpufreq
+            #
+    else
+        my_SCALING_DRIVER=UNKNOWN
+    fi
+}
+
+
 get_TURBO_HW_STATE() {
-    modprobe acpi_cpufreq
-    grep -q 010 /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_frequencies 
-    if [ $? -eq 0 ]; then
-        my_TURBO_HW_STATE=On 
-        my_TURBO_FREQ=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_frequencies | awk '{print $1}')
-    else
-        my_TURBO_HW_STATE=Off
-    fi
-}
 
-get_TURBO_ENGAGED() {
-    get_TURBO_HW_STATE
-    if [ "$my_TURBO_HW_STATE" = "On" ]; then
-        get_MAX_FREQ_ACTIVE
-        my_TURBO_ENGAGED=$(echo "$my_MAX_FREQ_ACTIVE" | grep 010 | awk '{print $1 " cores engaged"}')
-        if [ -z "$my_TURBO_ENGAGED" ]; then
-            # Nothing there, must not have any engaged -BEF-
-            my_TURBO_ENGAGED="No"
+    get_CPUFREQ_SCALING_DRIVER
+
+    if [ "$my_SCALING_DRIVER" = "intel_pstate" ]; then
+
+        if [ -e $INTEL_PSTATE_NO_TURBO_file ]; then
+            my_TURBO_HW_STATE=On
+        else
+            my_TURBO_HW_STATE=Off
         fi
-    else
-        my_TURBO_ENGAGED="No"
+
+        get_MAX_FREQ_AVAILABLE
+        my_TURBO_FREQ=$my_MAX_FREQ_AVAILABLE
+
+
+    elif [ "$my_SCALING_DRIVER" = "acpi-cpufreq" ]; then
+
+        grep -q 010 $acpi_cpufreq_SCALING_AVAILABLE_FREQUENCIES_file 
+        if [ $? -eq 0 ]; then
+            my_TURBO_HW_STATE=On 
+            my_TURBO_FREQ=$(cat $acpi_cpufreq_SCALING_AVAILABLE_FREQUENCIES_file | awk '{print $1}')
+        else
+            my_TURBO_HW_STATE=Off
+        fi
     fi
 }
 
-get_GOVERNER_ACTIVE() {
-    my_GOVERNER_ACTIVE=$(
+
+get_TURBO_OS_STATE() {
+
+    get_TURBO_HW_STATE
+
+    if [ "$my_TURBO_HW_STATE" = "On" ]; then
+
+        if [ "$my_SCALING_DRIVER" = "intel_pstate" ]; then
+
+            # 0 is the value in this file if turbo is engaged
+            grep -qw ^0 $INTEL_PSTATE_NO_TURBO_file
+            if [ $? -eq 0 ]; then
+                my_TURBO_OS_STATE="On"
+            else
+                my_TURBO_OS_STATE="Off"
+            fi
+
+        elif [ "$my_SCALING_DRIVER" = "acpi-cpufreq" ]; then
+
+            get_SCALING_MAX_FREQ_state
+            my_TURBO_OS_STATE=$(echo "$my_SCALING_MAX_FREQ_state" | grep 010 | awk '{print $1 " cores engaged"}')
+            if [ ! -z "$my_TURBO_OS_STATE" ]; then
+                my_TURBO_OS_STATE="On -- $my_TURBO_OS_STATE"
+            else
+                # Nothing there, must not have any engaged -BEF-
+                my_TURBO_OS_STATE="Off"
+            fi
+        fi
+
+    else
+        my_TURBO_OS_STATE="Off"
+    fi
+}
+
+
+get_SCALING_GOVERNER() {
+    my_SCALING_GOVERNER=$(
         cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor \
         | sort | uniq -c \
         | sed -e 's/^ *//' -e 's/ / cores using /'
         )
 }
 
-get_MAX_FREQ_ACTIVE() {
-    my_MAX_FREQ_ACTIVE=$(
+
+get_SCALING_MAX_FREQ_state() {
+    my_SCALING_MAX_FREQ_state=$(
         cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq \
         | sort | uniq -c \
         | sed -e 's/^ *//' -e 's/ / cores at /'
         )
 }
 
-get_MIN_FREQ_ACTIVE() {
-    my_MIN_FREQ_ACTIVE=$(
+get_SCALING_MIN_FREQ_state() {
+    my_SCALING_MIN_FREQ_state=$(
         cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_min_freq \
         | sort | uniq -c \
         | sed -e 's/^ *//' -e 's/ / cores at /'
@@ -59,18 +120,14 @@ get_MIN_FREQ_ACTIVE() {
 }
 
 get_MAX_FREQ_AVAILABLE() {
-    get_TURBO_HW_STATE
-    if [ "$my_TURBO_HW_STATE" = "On" ]; then
-        my_MAX_FREQ_AVAILABLE=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_frequencies | awk '{print $2}')
-    else
-        my_MAX_FREQ_AVAILABLE=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_frequencies | awk '{print $1}')
-    fi
+    my_MAX_FREQ_AVAILABLE=$(cat $CPUINFO_MAX_FREQ_file)
 }
 
+
 get_MIN_FREQ_AVAILABLE() {
-    modprobe acpi_cpufreq
-    my_MIN_FREQ_AVAILABLE=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_frequencies | awk '{print $NF}')
+    my_MIN_FREQ_AVAILABLE=$(cat $CPUINFO_MIN_FREQ_file)
 }
+
 
 get_CPU_ACTIVE_REAL_CORES() {
     my_CPU_ACTIVE_REAL_CORES_LIST=$(cat /sys/devices/system/cpu/cpu*/topology/thread_siblings_list \
@@ -84,6 +141,7 @@ get_CPU_ACTIVE_REAL_CORES() {
         | wc -l
         )
 }
+
 
 get_CPU_ACTIVE_HYPERTHREAD_CORES() {
     # This is the list of active hyperthread cores
@@ -101,14 +159,17 @@ get_CPU_ACTIVE_HYPERTHREAD_CORES() {
         )
 }
 
+
 get_CPU_SOCKETS() {
     my_CPU_SOCKETS_LIST=$(cat /sys/devices/system/cpu/cpu*/topology/physical_package_id  | sort -u)
     my_CPU_SOCKETS_COUNT=$(echo "$my_CPU_SOCKETS_LIST" | wc -l)
 }
 
+
 get_CPU_TOTAL_REAL_AND_HYPERTHREADING_CORES() {
     my_CPU_TOTAL_REAL_AND_HYPERTHREADING_CORES_COUNT=$(( $(/bin/ls /sys/devices/system/cpu/cpu*/online | wc -l) + 1 ))
 }
+
 
 get_CPU_TOTAL_HYPERTHREADING_CORES() {
 
@@ -118,16 +179,19 @@ get_CPU_TOTAL_HYPERTHREADING_CORES() {
     my_CPU_TOTAL_HYPERTHREADING_CORES_COUNT=$(( $my_CPU_TOTAL_REAL_AND_HYPERTHREADING_CORES_COUNT - $my_CPU_TOTAL_REAL_CORES_COUNT ))
 }
 
+
 get_CPU_TOTAL_REAL_CORES() {
     get_CPU_SOCKETS
     my_CPU_CORES_PER_SOCKET=$(grep 'cpu cores' /proc/cpuinfo | sort -u | awk '{print $NF}')
     my_CPU_TOTAL_REAL_CORES_COUNT=$(( $my_CPU_SOCKETS_COUNT * $my_CPU_CORES_PER_SOCKET ))
 }
 
+
 get_CPU_OFFLINE() {
 	my_CPU_OFFLINE_COUNT=$(grep -w 1 /sys/devices/system/cpu/cpu*/online | wc -l)
 	my_CPU_OFFLINE_LIST=$(grep -w 1 /sys/devices/system/cpu/cpu*/online | wc -l)
 }
+
 
 get_HYPERTHREADING_STATE() {
   
@@ -146,13 +210,15 @@ get_HYPERTHREADING_STATE() {
     fi
 }
 
+
 set_HYPERTHREADING_ON() {
     
     for core in /sys/devices/system/cpu/cpu*/online
     do
-        echo 1 > $core
+        echo -n 1 > $core
     done
 }
+
 
 set_HYPERTHREADING_OFF() {
 
@@ -176,10 +242,12 @@ set_HYPERTHREADING_STATE() {
     fi
 }
 
+
 get_C_STATE_LIMIT() {
     my_C_STATE_LIMIT=$(ps -A -o command | grep -v grep | grep set_dma_latency | awk '{print $NF}')
     test -z "$my_C_STATE_LIMIT" && my_C_STATE_LIMIT="Off"
 }
+
 
 get_C1E_STATE() {
     if [ ! -z "$C1EUTIL" -a -x "$C1EUTIL" ]; then
@@ -193,14 +261,16 @@ get_C1E_STATE() {
     fi
 }
 
+
 set_GOVERNER() {
     if [ ! -z $GOVERNER ]; then
         for core in /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
         do 
-            echo $GOVERNER > $core
+            echo -n $GOVERNER > $core
         done 
     fi
 }
+
 
 set_MIN_FREQ() {
     if [ -z $MIN_FREQ ]; then
@@ -208,14 +278,21 @@ set_MIN_FREQ() {
         MIN_FREQ=$my_MIN_FREQ_AVAILABLE
     fi
 
+    #
+    # Make sure the OS doesn't ignore us:
+    #
+    #   http://askubuntu.com/a/303957/144800
+    #
+    echo -n 1 > /sys/module/processor/parameters/ignore_ppc
+
     for core in /sys/devices/system/cpu/cpu*/cpufreq/scaling_min_freq
     do 
-        echo $MIN_FREQ > $core
+        echo -n $MIN_FREQ > $core
     done 
 }
 
-set_MAX_FREQ() {
 
+set_MAX_FREQ() {
     if [ -z "$MAX_FREQ" ]; then
         get_MAX_FREQ_AVAILABLE
         MAX_FREQ=$my_MAX_FREQ_AVAILABLE
@@ -226,21 +303,62 @@ set_MAX_FREQ() {
     #
     #   http://askubuntu.com/a/303957/144800
     #
-    echo 1 > /sys/module/processor/parameters/ignore_ppc
+    echo -n 1 > /sys/module/processor/parameters/ignore_ppc
 
     for core in $(/bin/ls /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq)
     do 
-        echo $MAX_FREQ > $core
+        echo -n $MAX_FREQ > $core
     done 
 }
+
 
 set_TURBO_ON() {
 
     get_TURBO_HW_STATE
 
-    if [ ! -z "$my_TURBO_FREQ" ]; then
+    if [ "$my_TURBO_HW_STATE" = "On" ]; then
+
+        if [ "$my_SCALING_DRIVER" = "intel_pstate" ]; then
+            echo -n 0 > $INTEL_PSTATE_NO_TURBO_file
+        fi 
+
+        #
+        # Set max freq to the proper freq for turbo use.
+        # get_TURBO_HW_STATE will choose the proper freq and set it to
+        # $my_TURBO_FREQ.
+        #
+        #   - With acpi-cpufreq, that's a frequency with '010' in it,
+        #     such as 2601000, for example.
+        #
+        #   - With intel_pstate, it's simply the highest freq for the
+        #     proc, such as 2600000, for example.
+        # 
         MAX_FREQ=$my_TURBO_FREQ
+
         set_MAX_FREQ
+    fi
+
+}
+
+
+set_TURBO_OFF() {
+
+    #
+    # Set max freq to the proper freq for non-turbo use.
+    #
+    #   - If not set by the user, set_MAX_FREQ will automatically
+    #     choose a proper freq.
+    # 
+    set_MAX_FREQ
+
+    get_TURBO_HW_STATE
+
+    if [ "$my_TURBO_HW_STATE" = "On" ]; then
+
+        if [ "$my_SCALING_DRIVER" = "intel_pstate" ]; then
+            echo -n 1 > $INTEL_PSTATE_NO_TURBO_file
+        fi
+
     fi
 }
 
@@ -253,6 +371,7 @@ set_C1E_DISABLED() {
     fi
 }
 
+
 set_C1E_ENABLED() {
     if [ ! -z "$C1EUTIL" -a -x "$C1EUTIL" ]; then
         $C1EUTIL -e >/dev/null 2>&1
@@ -260,6 +379,7 @@ set_C1E_ENABLED() {
         echo "Please install c1eutil.  Can't set c1e state"
     fi
 }
+
 
 set_C_STATE_LIMIT() {
 
