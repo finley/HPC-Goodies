5fc77e5acc926e79627815156220a430ba16c0b6
Brian Elliott Finley <brian@thefinleys.com>
2016-05-15 21:52:14 -0500

    - set_cpu_state: auto-unload acpi_pad to mitigate high cpu usage.

--- usr/lib/functions.sh
+++ usr/lib/functions.sh
@@ -12,7 +12,8 @@ SET_DMA_LATENCY=$(which set_dma_latency)
 cpu_map_cache_FILE="/var/cache/hpc-goodies/cpu_map_cache.db"
 
 acpi_cpufreq_SCALING_AVAILABLE_FREQUENCIES_file="/sys/devices/system/cpu/cpu0/cpufreq/scaling_available_frequencies"
-SCALING_DRIVER_file="/sys/devices/system/cpu/cpu0/cpufreq/scaling_driver"
+#SCALING_DRIVER_file="/sys/devices/system/cpu/cpu0/cpufreq/scaling_driver"
+SCALING_DRIVER_file=$(/bin/ls /sys/devices/system/cpu/cpu*/cpufreq/scaling_driver | head -n 1)
 INTEL_PSTATE_NO_TURBO_file="/sys/devices/system/cpu/intel_pstate/no_turbo"
 CPUINFO_MAX_FREQ_file="/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq"
 CPUINFO_MIN_FREQ_file="/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_min_freq"
@@ -23,6 +24,7 @@ CPUINFO_MIN_FREQ_file="/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_min_freq"
 #
 get_SCALING_DRIVER() {
     if [ ! -e "$SCALING_DRIVER_file" ]; then
+        modprobe -r acpi_pad
         modprobe acpi_cpufreq
             # if none loaded yet, let's load our preferred driver
     fi
